4853f8d19cafaeaa3ca04c297df63577012b8224
HALX99 <halx99@live.com>
2021-03-15 15:46:48 +0800

    Update build_osx.sh

--- NativeLibs/yasio/build_osx.sh
+++ NativeLibs/yasio/build_osx.sh
@@ -1,5 +1,5 @@
 mkdir -p build_osx && cd build_osx
-cmake -GXcode ../ -DYAISO_BUILD_NI=ON
+cmake -GXcode ../ -DYAISO_BUILD_NI=ON -DBUILD_SHARED_LIBS=ON
 cd ..
 cmake --build build_osx --config Release
 mkdir -p plugin_osx/Plugins/yasio.bundle/Contents/MacOS/
