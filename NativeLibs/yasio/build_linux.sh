c5a4a4dc5cb46394dc280a0e21ac6977a2f134d9
guoxiaodong.936 <guoxiaodong.936@bytedance.com>
2021-03-27 16:23:41 +0800

    fixup

--- NativeLibs/yasio/build_linux.sh
+++ NativeLibs/yasio/build_linux.sh
@@ -2,4 +2,4 @@ cmake -B build_linux64 -DBUILD_SHARED_LIBS=ON -DYAISO_BUILD_NI=ON
 cmake --build build_linux64 --config Release
 
 mkdir -p plugin_linux/Plugins/x86_64
-cp /y build_linux64/libyasio.so plugin_linux/Plugins/x86_64/
+cp build_linux64/libyasio.so plugin_linux/Plugins/x86_64/
