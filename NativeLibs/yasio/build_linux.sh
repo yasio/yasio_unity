a74401442536ea4fbdc32b8ee533037c70bf4d37
halx99 <halx99@live.com>
2021-03-27 16:11:33 +0800

    Add linux build

---
+++ NativeLibs/yasio/build_linux.sh
@@ -0,0 +1,5 @@
+cmake -B build_linux64 -DBUILD_SHARED_LIBS=ON -DYAISO_BUILD_NI=ON
+cmake --build build_linux64 --config Release
+
+mkdir -p plugin_linux/Plugins/x86_64
+copy /y build_linux64/libyasio.so plugin_linux/Plugins/x86_64/
