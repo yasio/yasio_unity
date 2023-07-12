a0f2b830e3aab98c922d8368debef05665a751db
halx99 <halx99@live.com>
2023-07-12 18:50:09 +0800

    Fix android ios build

--- NativeLibs/yasio/build_ios.sh
+++ NativeLibs/yasio/build_ios.sh
@@ -1,5 +1,5 @@
 mkdir -p build_ios && cd build_ios
-cmake -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=armv7;arm64" -GXcode ../
+cmake -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64" -GXcode ../
 cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_ios/Plugins/iOS/
