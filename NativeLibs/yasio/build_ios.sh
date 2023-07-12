856adcca5751ef4df53eed395142c94f5708a7e4
Deal(一线灵) <halx99@live.com>
2023-07-12 18:57:26 +0800

    Fix copy cmd

--- NativeLibs/yasio/build_ios.sh
+++ NativeLibs/yasio/build_ios.sh
@@ -3,5 +3,5 @@ cmake -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64" -GXcode ../
 cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_ios/Plugins/iOS/
-cp build_ios/Release-iphoneos/libyasio.a plugin_ios/Plugins/iOS/libyasio.a 
+cp build_ios/Release/libyasio.a plugin_ios/Plugins/iOS/libyasio.a 
 
