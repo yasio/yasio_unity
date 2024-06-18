bbeb8cdd9b5d8c4a1840eed1a082f83da824846e
halx99 <halx99@live.com>
2024-06-18 17:00:07 +0800

    Update build_ios.sh

--- NativeLibs/yasio/build_ios.sh
+++ NativeLibs/yasio/build_ios.sh
@@ -1,7 +1,4 @@
-mkdir -p build_ios && cd build_ios
-cmake -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64" -GXcode ../  -DYASIO_NO_DEPS=ON -DYASIO_ENABLE_NI=ON -DYASIO_ENABLE_KCP=ON -DYASIO_BUILD_TESTS=OFF
-cd ..
+cmake -B build_ios -GXcode -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64" -DYASIO_NO_DEPS=ON -DYASIO_ENABLE_NI=ON -DYASIO_ENABLE_KCP=ON -DYASIO_BUILD_TESTS=OFF
 cmake --build build_ios --config Release
 mkdir -p plugin_ios/Plugins/iOS/
 cp build_ios/Release/libyasio.a plugin_ios/Plugins/iOS/libyasio.a 
-
